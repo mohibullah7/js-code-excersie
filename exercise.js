fb2829373c1ceedc9c4d522297bbb67c453f0769
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-05-16 12:37:19 +0500

    next-changes
    today lec about string functioins

--- exercise.js
+++ exercise.js
@@ -2,3 +2,11 @@
 let abc = "this is my codespace";
 let value = abc.slice(11,20);
 console.log(value);
+
+let abcd = "this is my codespace";
+let valuee = abc.slice(11);
+console.log(valuee);
+
+let trim = "Hello World";
+let valueTrim = trim.slice(0,5);
+console.log(valueTrim);
